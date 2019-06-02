70a6d3d3ec70c8c7757a9096a9d2e918798a7179
Horst Gutmann <zerok@zerokspot.com>
2019-06-02 18:02:51 +0200

    Integrate goreleaser

--- Makefile
+++ Makefile
@@ -7,7 +7,10 @@ bin/kubeselect: $(shell find . -name '*.go') go.mod go.sum
 	cd cmd/kubeselect && go build -o ../../$@
 
 clean:
-	rm -rf bin
+	rm -rf bin dist
+
+snapshot:
+	goreleaser release --skip-publish --skip-validate --rm-dist --snapshot
 
 test:
 	go test -v ./...
@@ -15,4 +18,4 @@ test:
 install: bin/kubeselect
 	cp bin/kubeselect ~/bin/
 
-.PHONY: clean all test install
+.PHONY: clean all test install snapshot
--- cmd/kubeselect/main.go
+++ cmd/kubeselect/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/rs/zerolog"
 	"github.com/spf13/cobra"
 )
 
 var logger zerolog.Logger
+var (
+	version = "dev"
+	commit  = ""
+	date    = ""
+)
 
 var rootCmd = &cobra.Command{
 	Use: "kubeselect",
@@ -15,11 +22,35 @@ var rootCmd = &cobra.Command{
 	},
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		logger = zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr})
+		if v, _ := cmd.Flags().GetBool("version"); v {
+			printVersion()
+			os.Exit(0)
+		}
 	},
+}
+
+func printVersion() {
+	output := strings.Builder{}
+	output.WriteString(version)
+	if commit != "" || date != "" {
+		output.WriteString("  (")
+		if commit != "" {
+			output.WriteString("commit: " + commit)
+			if date != "" {
+				output.WriteString(", ")
+			}
+		}
+		if date != "" {
+			output.WriteString(fmt.Sprintf("built at %s", date))
+		}
+		output.WriteString(")")
+	}
+	fmt.Print(output.String())
 }
 
 func init() {
 	rootCmd.PersistentFlags().String("dotkube-dir", os.ExpandEnv("$HOME/.kube"), "Path to the .kube directory")
+	rootCmd.PersistentFlags().Bool("version", false, "Print version information")
 }
 
 func main() {
